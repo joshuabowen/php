cf40006e8432b8e25d1991acf26e9ca782a23182
Joshua Bowen <joshuabowen@Kimberlys-MacBook-Air.local>
2022-03-30 09:09:48 -0700

    I reset my testing file to a blank sheet to mess around with tomorrow.

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
 <p>This is my new web page that I am going to use to write php on.</p>
 <h2> all code below this heading is generated using php </h2>
 <?php
-$color = "white";
-$game = "life";
-Echo "I have a " . $color . " house.<br>";
-Echo "My favorite game is " .$game. " which I like to play very much.";
 ?>
 </body>
 </html>
