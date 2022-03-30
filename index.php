246ea8151864a1cfbbf7c47c8b97d3cd787a634e
Joshua Bowen <joshuabowen@Kimberlys-MacBook-Air.local>
2022-03-30 08:29:17 -0700

    changed the name of index.html to index.php. Also added a color and game veryable to my testing php file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<title> this is my page</title>
+<body>
+<h1> hello world</h1>
+<h2> my name is Joshua</h2>
+<p>This is my new web page that I am going to use to write php on.</p>
+<h2> all code below this heading is generated using php </h2>
+<?php
+$color = "white";
+$game = "life";
+Echo "I have a " . $color . " house.<br>";
+Echo "My favorite game is " .$game. " which I like to play very much.";
+?>
+</body>
+</html>
