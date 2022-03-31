b7889c62872932c37c46b1213d009b3671410da3
Joshua Bowen <joshuabowen@Kimberlys-MacBook-Air.local>
2022-03-31 15:38:37 -0700

    created a h2 project wear I took tow numbers displayed them and then showed the result when they are added together.

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 <p>This is my new web page that I am going to use to write php on.</p>
 <h2> all code below this heading is generated using php </h2>
 <?php
+echo "<h2> numbers exorcise </h2>";
+echo "<p> the following example will show the value of two numbers and then show the result when they are added together. This will show what happens when you take two varyables and add them.</p>";
+$x = 5;
+$y = 23;
+echo "the first number which is the variable x is " .$x."<br>";
+echo "The second number which is y is " .$y."<br>";
+echo "the final result of the two numbers x and y added together is ";
+Echo $x+$y;
+Echo ".";
 ?>
 </body>
 </html>
