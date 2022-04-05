7b5089204c705a9b8e244afab4ecda70b9a2319a
Joshua Bowen <joshuabowen@Kimberlys-MacBook-Air.local>
2022-04-05 13:45:10 -0700

    I made a little dirrectory with an index.php file which will be a linked list of all the little programs that I am working on.

---
+++ programs/index.php
@@ -0,0 +1,9 @@
+<html>
+<title> programs directory</title>
+<body>
+<?php
+Echo "<h1> programing index</h1>";
+Echo "<p>Hello everyone, this is my test page. On this page, you will find links to all my little projects that I am making in php. Feel free to take a look around and have fun.</p>";
+?>
+</body>
+</html>
